eef0e9d51515376dea51b7a78bbdd504ae102b94
Rhaysa Madruga <rysamad@gmail.com>
2020-11-01 16:40:32 -0300

    fix: size of loading

--- src/components/Loading/styles.js
+++ src/components/Loading/styles.js
@@ -7,11 +7,11 @@ const grow = keyframes`
 `;
 
 export const Pokeball = styled.div`
-  height: 250px;
-  width: 250px;
+  height: 125px;
+  width: 125px;
   border: 13px solid #383b4f;
   border-radius: 50%;
-  background-image: linear-gradient(#f51939 125px, white 125px);
+  background-image: linear-gradient(#f51939 62.5px, white 62.5px);
   position: relative;
   animation: ${grow} 2s infinite;
 
@@ -19,27 +19,27 @@ export const Pokeball = styled.div`
     position: absolute;
     content: '';
     height: 25px;
-    width: 250px;
+    width: 125px;
     background-color: #383b4f;
-    top: 112.5px;
+    top: 56.25px;
   }
 
   &:after {
     position: absolute;
     content: '';
-    height: 90px;
-    width: 90px;
+    height: 45px;
+    width: 45px;
     background-image: radial-gradient(
       #fff 0px,
-      #fff 10px,
+      #fff 5px,
+      #383b4f 5px,
       #383b4f 10px,
-      #383b4f 15px,
-      #fff 15px,
-      #fff 32px,
-      #383b4f 32px
+      #fff 10px,
+      #fff 16px,
+      #383b4f 16px
     );
     border-radius: 50%;
-    top: 80px;
-    left: 80px;
+    top: 40px;
+    left: 40px;
   }
 `;
