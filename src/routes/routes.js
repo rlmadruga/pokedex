134422e7fbe4bc51124ccaa26351dad46e49de24
Rhaysa Madruga <rysamad@gmail.com>
2020-10-31 23:03:26 -0300

    feat: Routes and Providers

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { AuthProvider } from '../contexts/AuthContext';
 import Signup from '../pages/Signup/Signup';
 import Login from '../pages/Login/Login';
 import Home from '../pages/Home/Home';
@@ -7,11 +8,13 @@ import Home from '../pages/Home/Home';
 export default function Routes() {
   return (
     <Router>
-      <Switch>
-        <Route exact path='/' component={Signup} />
-        <Route path='/login' component={Login} />
-        <Route path='/home' component={Home} />
-      </Switch>
+      <AuthProvider>
+        <Switch>
+          <Route exact path='/' component={Signup} />
+          <Route path='/login' component={Login} />
+          <Route path='/home' component={Home} />
+        </Switch>
+      </AuthProvider>
     </Router>
   );
 }
